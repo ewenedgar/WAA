f960cbc89ef3747245636a08d66e1af189d62ea0
Edgar Ntende <ewenedgar@gmail.com>
2023-10-15 12:19:32 -0500

    Add files via upload

---
+++ Lab08/src/test/java/edu/miu/cs/lab08/Lab08ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.lab08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
